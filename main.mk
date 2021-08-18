dadb42f91edb400e562f54cf77c53504e55653f7
Miles Whittaker <mj@whitta.dev>
2021-08-18 07:41:20 -0400

    Updated SRCDEPS

--- main.mk
+++ main.mk
@@ -6,7 +6,7 @@ LDFLAGS := -s -w
 OUT := $(BUILD)/$(GOOS)/$(GOARCH)
 PKG := $(shell $(GREP) "module\s+\K.+" go.mod)
 SRC := $(shell find . -name "*.go" -exec dirname {} \; | sort -u)
-SRCDEPS := $(shell go list -deps $(SRC) | grep -iPs "^git")
+SRCDEPS := $(shell go list -deps $(SRC) | grep -iPs "^(git|golang)")
 TST := $(shell find . -name "*_test.go" -exec dirname {} \; | sort -u)
 VERS := $(shell $(GREP) "const\s+Version\s+\=\s+\"\K[^\"]+" .)
 
