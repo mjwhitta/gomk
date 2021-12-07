c1f25e6a797191e9fce7609c70f99782b8c14f74
Miles Whittaker <mj@whitta.dev>
2021-12-07 15:43:04 -0500

    Fixed subshell problem

--- cmd.mk
+++ cmd.mk
@@ -1,5 +1,5 @@
 build-default: dir
-	$(foreach c,$(wildcard ./cmd/*),$(shell go build --ldflags "$(LDFLAGS)" -o "$(OUT)" --trimpath $c))
+	$(foreach c,$(wildcard ./cmd/*),@go build --ldflags "$(LDFLAGS)" -o "$(OUT)" --trimpath $c)
 
 debug-default: dir
-	$(foreach c,$(wildcard ./cmd/*),$(shell go build --gcflags all="-l -N" -o "$(OUT)" --trimpath $c))
+	$(foreach c,$(wildcard ./cmd/*),@go build --gcflags all="-l -N" -o "$(OUT)" --trimpath $c)
